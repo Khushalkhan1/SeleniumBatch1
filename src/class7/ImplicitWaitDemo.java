08802b4d29adf91f2bbc0de44a06809ec9de6021
khushalkhan <khushalkhanpaktiawal321@gmail.com>
2023-05-16 05:11:24 +0300

    SeleniumBatch1

---
+++ src/class7/ImplicitWaitDemo.java
@@ -0,0 +1,29 @@
+package class7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ImplicitWaitDemo {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        WebElement enableButton=driver.findElement(By.cssSelector("#input-example>button"));
+        enableButton.click();
+        WebElement text= driver.findElement(By.id("message"));
+        System.out.println("Is the Text message displayed ? : "+text.isDisplayed());
+        WebElement removeButton= driver.findElement(By.cssSelector("#checkbox-example>button"));
+        removeButton.getText();
+
+
+
+    }
+}
---
+++ src/class7/WindowHandleDemo1.java
@@ -0,0 +1,37 @@
+package class7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class WindowHandleDemo1 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://accounts.google.com/signup");
+        driver.manage().window().maximize();
+        String mainPageHandle = driver.getWindowHandle();
+        System.out.println(mainPageHandle);
+        WebElement helpLink = driver.findElement(By.linkText("Yardım"));
+        helpLink.click();
+        Set<String> windowHandle = driver.getWindowHandles();
+        System.out.println(windowHandle.size());
+        Iterator<String> iterator = windowHandle.iterator();
+        mainPageHandle = iterator.next();
+        String childWindowHandle = iterator.next();
+        System.out.println(childWindowHandle);
+        driver.findElement(By.id("firstName")).sendKeys("JOINCORETECH");
+        driver.switchTo().window(childWindowHandle);
+        driver.findElement(By.cssSelector("input[name='q'")).sendKeys("Login Help");
+        driver.switchTo().window(mainPageHandle);
+
+
+
+    }
+
+}
